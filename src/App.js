b57fe342e797f358768126b9fec1be310dc9d689
Rejens <rejensraya@gmail.com>
2023-06-16 18:50:32 +0545

    correct

--- src/App.js
+++ src/App.js
@@ -5,35 +5,35 @@ import Header from "./components/Header/Header";
 import DescriptionImageText from "./components/DescriptionImgaeText/DescriptionImageText";
 
 const concepts = [
-  {
-    title: "Components",
-    image: componentsImage,
-    description:
-      "Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.",
-  },
-  {
-    title: "State",
-    image: stateImage,
-    description:
-      "State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.",
-  },
-  {
-    title: "Events",
-    image: eventsImage,
-    description:
-      "Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.",
-  },
+   {
+      title: "Components",
+      image: componentsImage,
+      description:
+         "Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.",
+   },
+   {
+      title: "State",
+      image: stateImage,
+      description:
+         "State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.",
+   },
+   {
+      title: "Events",
+      image: eventsImage,
+      description:
+         "Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.",
+   },
 ];
 
 function App() {
-  return (
-    <div>
-      <Header></Header>
-      <ul id="concepts">
-        <DescriptionImageText></DescriptionImageText>
-      </ul>
-    </div>
-  );
+   return (
+      <div>
+         <Header />
+         <ul id="concepts">
+            <DescriptionImageText concept={concepts} />
+         </ul>
+      </div>
+   );
 }
 
 export default App;
--- src/components/DescriptionImgaeText/DescriptionImageText.js
+++ src/components/DescriptionImgaeText/DescriptionImageText.js
@@ -1,23 +1,30 @@
 import React from "react";
-import Concept from "./components/Concept/Concept";
-export default function DescriptionImageText(props) {
-  return (
-    <div>
-      <Concept
-        image={concepts[0].image}
-        title={concepts[0].title}
-        description={concepts[0].description}
-      ></Concept>
-      <Concept
-        image={concepts[1].image}
-        title={concepts[1].title}
-        description={concepts[1].description}
-      ></Concept>
-      <Concept
-        image={concepts[2].image}
-        title={concepts[2].title}
-        description={concepts[2].description}
-      ></Concept>
-    </div>
-  );
+import Concept from "../Concept/Concept";
+export default function DescriptionImageText({ concept }) {
+   return (
+      <div>
+         {/* <Concept
+            image={props.concepts[0].image}
+            title={props.concepts[0].title}
+            description={props.concepts[0].description}
+         />
+         <Concept
+            image={props.concepts[1].image}
+            title={props.concepts[1].title}
+            description={props.concepts[1].description}
+         />
+         <Concept
+            image={props.concepts[2].image}
+            title={props.concepts[2].title}
+            description={props.concepts[2].description}
+         /> */}
+         {concept.map((item) => (
+            <Concept
+               image={item.image}
+               title={item.title}
+               description={item.description}
+            />
+         ))}
+      </div>
+   );
 }
